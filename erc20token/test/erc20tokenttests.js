12d03db0eb00708af92fb8f0f2d910e30db42f7c
Joel Makara <makarajm@gmail.com>
2019-03-12 10:58:40 +0200

    WIP writting tests

--- erc20token/test/erc20tokenttests.js
+++ erc20token/test/erc20tokenttests.js
@@ -1,7 +1,21 @@
+
+const erc20token = artifacts.require("erc20token");
+
 contract('erc20tokenttests', function(accounts) {
   it("should assert true", function(done) {
-    var erc20tokenttests = erc20tokenttests.deployed();
+    erc20token.deployed();
     assert.isTrue(true);
     done();
   });
+
+  it("should put 10000000 ERC20 Tokens in the first account", () =>
+      erc20token.deployed()
+      .then(instance => instance.getBalance.call(accounts[0]))
+      .then(balance => {
+        assert.equal(
+          balance.valueOf(),
+          10000000,
+          "10000 wasn't in the first account"
+        );
+      }));
 });
