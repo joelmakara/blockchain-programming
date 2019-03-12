2603121bd6e65474b650dc7ce489cdbb04348cd1
Joel Makara <makarajm@gmail.com>
2019-03-12 10:49:20 +0200

    Migrate smart contract onto Ganache, and new up test file

---
+++ erc20token/test/erc20tokenttests.js
@@ -0,0 +1,7 @@
+contract('erc20tokenttests', function(accounts) {
+  it("should assert true", function(done) {
+    var erc20tokenttests = erc20tokenttests.deployed();
+    assert.isTrue(true);
+    done();
+  });
+});
